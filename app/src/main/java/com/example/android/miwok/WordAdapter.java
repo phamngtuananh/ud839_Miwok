3a3b409ba245e37647649e6b7f49429a8a9c7c0b
phamngtuananh <phamngtuananh@gmail.com>
2018-08-10 18:56:33 +0800

    Add

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,4 +1,57 @@
 package com.example.android.miwok;
 
-public class WordAdapter {
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewDebug;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+    private int mColor;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
+        super(context, 0, words);
+        mColor = ContextCompat.getColor(getContext(), colorResourceId);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        /** Get view and set background color */
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        textContainer.setBackgroundColor(mColor);
+        final Word currentWord = getItem(position);
+
+        /** Set data */
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        nameTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView numberTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        numberTextView.setText(currentWord.getDefaultTranslation());
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
+        /** Only show the image if the word has one */
+        if (currentWord.hasImage()) {
+            imageView.setImageResource(currentWord.getImageResourceId());
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
+
+        return listItemView;
+    }
 }
